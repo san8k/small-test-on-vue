14b857690c4c0b65371f62085f1475ab484c35ac
Alexander <alrttv@gmail.com>
2019-01-28 14:24:54 +0300

    Потренировался с анимированием появления

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <form @submit.prevent="onSubmit()" v-if="questionIndex < answersList.length">
-      <div >
-        <app-question :text="changeQuestion.title">
-        </app-question>
-        <app-answers :answersList="changeQuestion" @onanswer="saveAnswer($event)">
-        </app-answers>
-      </div>
-      <app-submit :type="`submit`" :text="`Ответить`" :disabled="isSubmtDisabled"></app-submit>
-    </form>
-    <app-results :questionsList="answersList" :answersList="userAnswers" v-else>
-
-    </app-results>
+    <transition name="fade" mode="out-in">
+      <form @submit.prevent="onSubmit()" v-if="questionIndex < answersList.length">
+        <transition name="slide" mode="out-in" appear>
+          <div :key="questionIndex">
+            <app-question :text="changeQuestion.title">
+            </app-question>
+            <app-answers :answersList="changeQuestion" @onanswer="saveAnswer($event)">
+            </app-answers>
+          </div>
+        </transition>
+        <app-submit :type="`submit`" :text="`Ответить`" :disabled="isSubmtDisabled"></app-submit>
+      </form>
+      <app-results :questionsList="answersList" :answersList="userAnswers" v-else>
+      </app-results>
+    </transition>
   </div>
 </template>
 
@@ -85,4 +88,64 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
+
+
+.slide-enter-active {
+  animation: slideIn 0.5s;
+}
+
+.slide-leave-active {
+  animation: slideOut 0.5s;
+}
+
+.fade-enter-active {
+  animation: fadeIn 0.6s;
+}
+
+.fade-leave-active {
+  animation: fadeOut 0.8s;
+}
+
+@keyframes slideIn {
+  from {
+    opacity: 0;
+    transform: translateX(15vw);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+@keyframes slideOut {
+  from {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateX(-15vw);
+  }
+}
+
+@keyframes fadeIn {
+  from {
+    transform: translateY(-15vh);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes fadeOut {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+}
+
 </style>
